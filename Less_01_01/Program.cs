33e2b97b9d6d293e5ce2e8ae8eeeb5e9e1e34b75
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-08-28 21:29:31 +0400

    Less 01 01

---
+++ Less_01_01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b) Console.Write($"max = {a}");
+else Console.Write($"max = {b}");
