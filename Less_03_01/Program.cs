55d24ab63627bb40177b0d3b932b5b3ba6c408f2
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-08-30 21:06:01 +0400

    Less 03 01

---
+++ Less_03_01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите пятизначное число: ");
+string str = Console.ReadLine()!;
+
+string Expand(string str) 
+{
+    string newStr = "";
+    for (int i = str.Length; i > 0; i--)
+    {
+        newStr = newStr + str[i-1];
+    }
+    return newStr;
+}
+
+if (str == Expand(str)) Console.Write("Палиндром");
+else Console.Write("Не палиндром");
