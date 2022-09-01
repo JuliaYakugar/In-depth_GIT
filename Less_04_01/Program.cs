50910a009b4fd704d328368b24423fb31f9c60aa
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-01 18:35:19 +0400

    Less 04 01

---
+++ Less_04_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+double A = double.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+double B = double.Parse(Console.ReadLine()!);
+
+double p(double a, double b)
+{
+    return Math.Pow(a, b);
+}
+
+Console.Write($"Ответ: {p(A, B)}");
