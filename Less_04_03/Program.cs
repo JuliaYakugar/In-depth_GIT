7b889b35da5adc28fb192347be55d1ef729ea99e
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-01 20:10:41 +0400

    Less 04 03

---
+++ Less_04_03/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] Mas()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+int[] array = Mas();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}");
+    if(i != array.Length-1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.Write("]");
