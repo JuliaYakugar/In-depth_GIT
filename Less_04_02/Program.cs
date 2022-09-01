4cd766124c7baa9279ffdf91995595f4935d7dba
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-01 18:59:54 +0400

    Less 04 02

---
+++ Less_04_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+void Sum(int a)
+{
+    if (a > 0)
+    {
+        sum += a % 10;
+        Sum(a/10);
+    }
+}
+Sum(A);
+Console.Write($"Сумма цифр: {sum}");
