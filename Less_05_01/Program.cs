1268f1bb35d9b869bae16e81786004bac29d856b
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-07 00:23:47 +0400

    Less 05 01

---
+++ Less_05_01/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = new int[10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i]%2 == 0) count++;
+}
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}");
+    if(i != array.Length-1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
+
+Console.Write($"Четных: {count}");
