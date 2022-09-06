7deac1eecbe488ebad0b38236e6872697b3dca7d
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-07 00:24:04 +0400

    Less 05 02

---
+++ Less_05_02/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = new int[10];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    if (i%2 != 0) sum += array[i];
+}
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}");
+    if(i != array.Length-1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
+
+Console.Write($"Сумма: {sum}");
