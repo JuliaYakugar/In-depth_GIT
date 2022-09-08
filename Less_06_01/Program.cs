adf66aac59c4a818bf1156f2e928f71d472d55fc
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-08 20:15:57 +0400

    Less 06 01

---
+++ Less_06_01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Сколько чисел вы будете вводить (М): ");
+int[] array = new int [int.Parse(Console.ReadLine()!)];
+
+int pol = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите число №{i+1}: ");
+    array[i] = int.Parse(Console.ReadLine()!);
+    if (array[i] > 0) pol++;
+}
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}");
+    if(i != array.Length-1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
+
+Console.WriteLine($"Больше нуля: {pol}");
