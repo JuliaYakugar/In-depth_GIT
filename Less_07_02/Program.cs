804d68e387e8f3b6f4849a5d33adf5c0a8579723
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-22 14:05:04 +0400

    Less 07 02

---
+++ Less_07_02/Program.cs
@@ -0,0 +1,39 @@
+﻿int m = 3;
+int n = 4;
+
+int[,] array = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(10);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+void Search(int a, int b)
+{
+    if ((a <= m) && (b <= n)) 
+    {
+        Console.Write(array[a-1, b-1]);
+    }
+    else
+    {
+        Console.Write("Такого числа нет");
+    }
+}
+
+Console.Write($"В каком столбце вас интересует значение? ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write($"В какой строке вас интересует значение? ");
+int a = int.Parse(Console.ReadLine()!);
+Search(a, b);
