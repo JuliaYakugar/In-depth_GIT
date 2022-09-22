43b4f52ae1666152d5b2ff507bf05f40fd2d89ab
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2022-09-22 12:04:45 +0400

    Less 07 01

---
+++ Less_07_01/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write($"Введите количество столбцов: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write($"Введите количество строк: ");
+int n = int.Parse(Console.ReadLine()!);
+
+double[,] array = new double[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Math.Round(new Random().NextDouble() * (10 - (-10)) + (-10), 1);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
